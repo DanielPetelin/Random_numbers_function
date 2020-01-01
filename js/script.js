eaae6a5bc63be26e7ed50bf8be54efa522660382
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-01 14:00:49 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+//Функция, которая будет принемать значения для max, min.
 document.querySelector('button').onclick = function() {
     var l = document.querySelector('#length').value;
     var min = document.querySelector('#min').value;
     var max = document.querySelector('#max').value;
     var result = [];
 
+//Цикл, записывающий результат.
     for (var i = 0; i < l; i++) {
         result.push(randomInteger(min, max));
     };
@@ -11,8 +13,9 @@ document.querySelector('button').onclick = function() {
     document.querySelector('#out').innerHTML = result;
     document.querySelector('#out-input').value = result;
     return result;
-}
+};
 
+//Функция генерирующая рандомные числа.
 function randomInteger(max, min) {
     var rand = min - 0.5 + Math.random() * (max - min + 1);
     rand = Math.round(rand);
