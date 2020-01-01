c8cdcd2953978b7c5b98f2d2ec61c8baded4446c
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-01 13:30:38 +0300

    fun

--- js/script.js
+++ js/script.js
@@ -2,4 +2,11 @@ document.querySelector('button').onclick = function() {
     var l = document.querySelector('#length');
     var min = document.querySelector('#min');
     var max = document.querySelector('#max');
+    var result = [];
+}
+
+function randomInteger(max, min) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1);
+    rand = Math.round(rand);
+    return rand;
 }
