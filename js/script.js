3476905dde52564a75aa6570278be09b5dccd9ce
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-01 13:27:19 +0300

    #

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.querySelector('button').onclick = function() {
+    var l = document.querySelector('#length');
+    var min = document.querySelector('#min');
+    var max = document.querySelector('#max');
+}
